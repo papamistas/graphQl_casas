44e678d7389c95c5b3b9e3224023a39279c469db
Leonel-Domingos <leonel.domingos@quad-systems.com>
2019-03-29 21:55:45 +0000

    PRoxy working 8080 to 7000...now have to use axios for requests

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,18 @@ module.exports = {
     host: "0.0.0.0",
     port: 8080, // CHANGE YOUR PORT HERE!
     //https: true,
-    hotOnly: false
-  }
+    hotOnly: false,
+    proxy: {
+      '^/auth': {
+        target: 'http://[::1]:7000',
+        ws: true,
+        changeOrigin: true
+      },
+      '^/foo': {
+        target: 'casasdapraia.com'
+      }
+    }
+  },
+
+
 };
